6c5c1dad927437d077b974653cedc388b25b0856
SHREYA D <shreyagowdad852@gmail.com>
2026-08-05 19:31:39 +0530

    Add solution for finding largest element in array

---
+++ 01-Arrays/find_largest.py
@@ -0,0 +1,16 @@
+def find_largest(arr):
+    largest = arr[0]
+
+    for num in arr:
+        if num > largest:
+            largest = num
+
+    return largest
+
+
+arr = [10, 5, 25, 8, 30, 15]
+
+result = find_largest(arr)
+
+print("Array:", arr)
+print("Largest element:", result)
