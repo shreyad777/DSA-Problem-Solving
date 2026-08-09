0240db8667218f33c40963b6bb25567c58c2cf5e
shreyad777 <shreyagowdad852@gmail.com>
2026-08-09 19:23:14 +0530

    Add string compression solution

---
+++ 02-Strings/string_compression.py
@@ -0,0 +1,28 @@
+def compress_string(text):
+
+    if not text:
+        return ""
+
+    result = ""
+    count = 1
+
+    for i in range(len(text) - 1):
+
+        if text[i] == text[i + 1]:
+            count += 1
+
+        else:
+            result += text[i] + str(count)
+            count = 1
+
+    result += text[-1] + str(count)
+
+    return result
+
+
+text = "xxxxxyyzz"
+
+result = compress_string(text)
+
+print("Original string:", text)
+print("Compressed string:", result)
