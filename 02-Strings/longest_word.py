ac3a53989b4d66020b797572559d5dc12a585e85
shreyad777 <shreyagowdad852@gmail.com>
2026-08-09 19:16:59 +0530

    Add longest word solution

---
+++ 02-Strings/longest_word.py
@@ -0,0 +1,20 @@
+def find_longest_word(text):
+
+    words = text.split()
+
+    longest = ""
+
+    for word in words:
+
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
+
+
+text = "I love programming"
+
+result = find_longest_word(text)
+
+print("Sentence:", text)
+print("Longest word:", result)
