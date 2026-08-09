6533bf3fac52be961cf2eccc61f547b5738b2fc3
shreyad777 <shreyagowdad852@gmail.com>
2026-08-09 19:02:17 +0530

    Add character frequency solution

---
+++ 02-Strings/character_frequency.py
@@ -0,0 +1,21 @@
+def character_frequency(text):
+
+    frequency = {}
+
+    for char in text:
+
+        if char in frequency:
+            frequency[char] += 1
+
+        else:
+            frequency[char] = 1
+
+    return frequency
+
+
+text = "hello"
+
+result = character_frequency(text)
+
+print("String:", text)
+print("Character frequency:", result)
