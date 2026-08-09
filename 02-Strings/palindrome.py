937ba9148ab9189ec3eb5ac71d3d5c045cabbefe
SHREYA D <shreyagowdad852@gmail.com>
2026-08-09 06:51:31 +0530

    Add palindrome checking function and test

---
+++ 02-Strings/palindrome.py
@@ -0,0 +1,43 @@
+def is_palindrome(text):
+
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+
+        if text[left] != text[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+text = "madam"
+
+if is_palindrome(text):
+    print("Palindrome")
+else:
+    print("Not a palindrome")def is_palindrome(text):
+
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+
+        if text[left] != text[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+text = "madam"
+
+if is_palindrome(text):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
