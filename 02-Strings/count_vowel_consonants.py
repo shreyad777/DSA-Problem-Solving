0844127a5cbd4856c386f19fe6d31c2d9da3ecb9
shreyad777 <shreyagowdad852@gmail.com>
2026-08-09 18:28:01 +0530

    Add vowel and consonant counting solution

---
+++ 02-Strings/count_vowel_consonants.py
@@ -0,0 +1,24 @@
+def count_vowels_consonants(text):
+
+    vowels = "aeiou"
+
+    vowel_count = 0
+    consonant_count = 0
+
+    for char in text:
+
+        if char.lower() in vowels:
+            vowel_count += 1
+
+        elif char.isalpha():
+            consonant_count += 1
+
+    return vowel_count, consonant_count
+
+text = "hello"
+
+vowels, consonants = count_vowels_consonants(text)
+
+print("String:", text)
+print("Vowels:", vowels)
+print("Consonants:", consonants)
