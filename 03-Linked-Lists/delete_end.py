95b26cd1ec29b31159e42a929114e007cf40340c
shreyad777 <shreyagowdad852@gmail.com>
2026-08-10 23:40:40 +0530

    Add delete last node solution

---
+++ 03-Linked-Lists/delete_end.py
@@ -0,0 +1,54 @@
+# Problem 26: Delete the Last Node
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def delete_end(head):
+
+    if head is None:
+        return None
+
+    if head.next is None:
+        return None
+
+    current = head
+
+    while current.next.next is not None:
+        current = current.next
+
+    current.next = None
+
+    return head
+
+
+# Create linked list
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+head = node1
+
+
+# Delete the last node
+head = delete_end(head)
+
+
+# Traverse the linked list
+current = head
+
+while current is not None:
+
+    print(current.data, end=" ")
+
+    current = current.next
+    
