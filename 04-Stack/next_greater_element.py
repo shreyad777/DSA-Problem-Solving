7b088460e5804b6ffad295123a1cdb905451e147
shreyad777 <shreyagowdad852@gmail.com>
2026-08-11 00:55:10 +0530

    Add next greater element solution

---
+++ 04-Stack/next_greater_element.py
@@ -0,0 +1,25 @@
+
+def next_greater_element(arr):
+
+    result = [-1] * len(arr)
+
+    stack = []
+
+    for i in range(len(arr)):
+
+        while stack and arr[i] > arr[stack[-1]]:
+
+            index = stack.pop()
+
+            result[index] = arr[i]
+
+        stack.append(i)
+
+    return result
+
+arr = [4, 5, 2, 10, 8]
+
+result = next_greater_element(arr)
+
+print("Array:", arr)
+print("Next greater elements:", result)
