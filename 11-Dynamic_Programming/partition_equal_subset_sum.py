fb3174a72472e432c10361c00d592aae0a4dd0d2
shreyad777 <shreyagowdad852@gmail.com>
2026-08-19 23:37:00 +0530

    Added a partition of equal subset sum

---
+++ 11-Dynamic_Programming/partition_equal_subset_sum.py
@@ -0,0 +1,38 @@
+# Question 118: Partition Equal Subset Sum
+
+
+def can_partition(nums):
+
+    total_sum = sum(nums)
+
+    # Odd total cannot be divided equally
+    if total_sum % 2 != 0:
+        return False
+
+    target = total_sum // 2
+
+    # dp[s] tells whether sum s is possible
+    dp = [False] * (target + 1)
+
+    dp[0] = True
+
+    for num in nums:
+
+        # Traverse backwards
+        for current_sum in range(
+            target,
+            num - 1,
+            -1
+        ):
+
+            dp[current_sum] = (
+                dp[current_sum]
+                or dp[current_sum - num]
+            )
+
+    return dp[target]
+
+
+nums = [1, 5, 11, 5]
+
+print("Can partition:", can_partition(nums))
