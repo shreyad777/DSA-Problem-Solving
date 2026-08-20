df7854c743c7cd48a9527998b2402cdbc37cdf2f
shreyad777 <shreyagowdad852@gmail.com>
2026-08-20 21:31:27 +0530

    Added a word breaker

---
+++ 11-Dynamic_Programming/word_break.py
@@ -0,0 +1,29 @@
+def word_break(s, wordDict):
+
+    word_set = set(wordDict)
+
+    n = len(s)
+
+    # dp[i] means:
+    # first i characters can be segmented
+    dp = [False] * (n + 1)
+
+    dp[0] = True
+
+    for i in range(1, n + 1):
+
+        for j in range(i):
+
+            if dp[j] and s[j:i] in word_set:
+
+                dp[i] = True
+                break
+
+    return dp[n]
+
+
+s = "leetcode"
+
+wordDict = ["leet", "code"]
+
+print("Can word be segmented:", word_break(s, wordDict))
