04f30fd66e5538e4e2debb47d7d8ac163fab05dc
shreyad777 <shreyagowdad852@gmail.com>
2026-08-23 23:54:45 +0530

    Add house robber II dynamic programming

---
+++ 11-Dynamic_Programming/house_robber_ii.py
@@ -0,0 +1,45 @@
+# Question 127: House Robber II
+
+
+def rob_linear(nums):
+
+    previous_two = 0
+    previous_one = 0
+
+    for money in nums:
+
+        current = max(
+            previous_one,
+            previous_two + money
+        )
+
+        previous_two = previous_one
+        previous_one = current
+
+    return previous_one
+
+
+def rob(nums):
+
+    n = len(nums)
+
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return nums[0]
+
+    # Case 1:
+    # Do not rob the first house
+    case_one = rob_linear(nums[1:])
+
+    # Case 2:
+    # Do not rob the last house
+    case_two = rob_linear(nums[:-1])
+
+    return max(case_one, case_two)
+
+
+nums = [2, 3, 2]
+
+print("Maximum money:", rob(nums))
