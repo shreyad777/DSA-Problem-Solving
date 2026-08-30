329d7717327e528e5f3ef6d014fe9e6918818b80
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:32:46 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/climbing_stairs.py
+++ 11-Dynamic_Programming/climbing_stairs.py
@@ -7,15 +7,11 @@ def climb_stairs(n):
     previous_one = 2
 
     for i in range(3, n + 1):
-
         current = previous_one + previous_two
-
         previous_two = previous_one
         previous_one = current
 
     return previous_one
-
-
 n = 5
 
 print("Number of ways:", climb_stairs(n))
