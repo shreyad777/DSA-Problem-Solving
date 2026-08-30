b43b29c41b5b1302ddca6c44f99e46849b065357
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:45:22 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/longest_common_subsequence.py
+++ 11-Dynamic_Programming/longest_common_subsequence.py
@@ -2,29 +2,19 @@ def longest_common_subsequence(text1, text2):
 
     m = len(text1)
     n = len(text2)
-
-    # DP table
     dp = [
         [0] * (n + 1)
         for _ in range(m + 1)
     ]
-
-    # Fill the table
     for i in range(1, m + 1):
-
         for j in range(1, n + 1):
-
             if text1[i - 1] == text2[j - 1]:
-
                 dp[i][j] = dp[i - 1][j - 1] + 1
-
             else:
-
                 dp[i][j] = max(
                     dp[i - 1][j],
                     dp[i][j - 1]
                 )
-
     return dp[m][n]
 
 
