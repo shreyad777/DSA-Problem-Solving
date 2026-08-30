f6c674559961d0535865c254fcb8f6b6f8c45309
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:52:08 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/unique_paths.py
+++ 11-Dynamic_Programming/unique_paths.py
@@ -1,20 +1,12 @@
 def unique_paths(m, n):
-
-    # Create DP table
     dp = [
         [0] * n
         for _ in range(m)
     ]
-
-    # First row
     for j in range(n):
         dp[0][j] = 1
-
-    # First column
     for i in range(m):
         dp[i][0] = 1
-
-    # Fill remaining cells
     for i in range(1, m):
 
         for j in range(1, n):
@@ -25,9 +17,6 @@ def unique_paths(m, n):
             )
 
     return dp[m - 1][n - 1]
-
-
 m = 3
 n = 3
-
 print("Number of unique paths:", unique_paths(m, n))
