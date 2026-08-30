df31af91854cf2d102fd986f56d73657f988a6d3
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:34:56 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/coin_change.py
+++ 11-Dynamic_Programming/coin_change.py
@@ -9,22 +9,15 @@ def coin_change(coins, amount):
     dp[0] = 0
 
     for current_amount in range(1, amount + 1):
-
         for coin in coins:
-
             if coin <= current_amount:
-
                 dp[current_amount] = min(
                     dp[current_amount],
                     dp[current_amount - coin] + 1
                 )
-
     if dp[amount] == impossible:
         return -1
-
     return dp[amount]
-
-
 coins = [1, 2, 5]
 amount = 11
 
