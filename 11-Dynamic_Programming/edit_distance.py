95b42e2e2a2e4ab7dfb1fc60b51e6fa19c2051ad
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:40:22 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/edit_distance.py
+++ 11-Dynamic_Programming/edit_distance.py
@@ -3,21 +3,16 @@ def edit_distance(word1, word2):
     m = len(word1)
     n = len(word2)
 
-    # Create DP table
     dp = [
         [0] * (n + 1)
         for _ in range(m + 1)
     ]
 
-    # Convert word1 to empty string
     for i in range(m + 1):
         dp[i][0] = i
-
-    # Convert empty string to word2
     for j in range(n + 1):
         dp[0][j] = j
 
-    # Fill the table
     for i in range(1, m + 1):
 
         for j in range(1, n + 1):
