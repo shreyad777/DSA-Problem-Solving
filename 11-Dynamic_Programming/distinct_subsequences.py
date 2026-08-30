52ced8e5c7050967443ecec5e54d6d634d0ef249
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:38:31 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/distinct_subsequences.py
+++ 11-Dynamic_Programming/distinct_subsequences.py
@@ -7,9 +7,6 @@ def num_distinct(s, t):
         [0] * (n + 1)
         for _ in range(m + 1)
     ]
-
-    # Empty t can be formed in exactly
-    # one way: choose nothing.
     for i in range(m + 1):
         dp[i][0] = 1
 
@@ -18,16 +15,12 @@ def num_distinct(s, t):
         for j in range(1, n + 1):
 
             if s[i - 1] == t[j - 1]:
-
-                # Use the character OR skip it
                 dp[i][j] = (
                     dp[i - 1][j - 1]
                     + dp[i - 1][j]
                 )
 
             else:
-
-                # Skip s[i - 1]
                 dp[i][j] = dp[i - 1][j]
 
     return dp[m][n]
