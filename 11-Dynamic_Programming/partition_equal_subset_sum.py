0485e95b3fbe6db8d1b6d1a4d33fdd343bb6a469
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:50:08 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/partition_equal_subset_sum.py
+++ 11-Dynamic_Programming/partition_equal_subset_sum.py
@@ -1,21 +1,15 @@
 def can_partition(nums):
 
     total_sum = sum(nums)
-
-    # Odd total cannot be divided equally
     if total_sum % 2 != 0:
         return False
 
     target = total_sum // 2
-
-    # dp[s] tells whether sum s is possible
     dp = [False] * (target + 1)
 
     dp[0] = True
 
     for num in nums:
-
-        # Traverse backwards
         for current_sum in range(
             target,
             num - 1,
@@ -28,8 +22,5 @@ def can_partition(nums):
             )
 
     return dp[target]
-
-
 nums = [1, 5, 11, 5]
-
 print("Can partition:", can_partition(nums))
