6e781d8985e030a933aa644a89d908e7b3e1a7ff
shreyad777 <shreyagowdad852@gmail.com>
2026-08-30 23:43:09 +0530

    Modified the updated solution

--- 11-Dynamic_Programming/jump_game_ii.py
+++ 11-Dynamic_Programming/jump_game_ii.py
@@ -10,22 +10,16 @@ def jump(nums):
     farthest = 0
 
     for i in range(n - 1):
-
-        # Farthest position reachable
-        # from the current range
         farthest = max(
             farthest,
             i + nums[i]
         )
 
-        # We have reached the end
-        # of the current jump range
         if i == current_end:
 
             jumps += 1
             current_end = farthest
 
-            # Already reached the end
             if current_end >= n - 1:
                 break
 
