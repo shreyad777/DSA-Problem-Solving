68de017ad1ffb028949d671caaf039ec5eced4e5
shreyad777 <shreyagowdad852@gmail.com>
2026-08-31 12:13:02 +0530

    Modified the existing solution and commited the updated changes

--- 06-Trees/postorder_traversal.py
+++ 06-Trees/postorder_traversal.py
@@ -1,12 +1,8 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
-
-# Create tree
 root = Node(10)
 
 root.left = Node(5)
@@ -15,23 +11,13 @@ root.right = Node(20)
 root.left.left = Node(3)
 root.left.right = Node(7)
 
-
-# Postorder traversal
 def postorder(root):
 
     if root is None:
         return
 
-    # Visit left subtree
     postorder(root.left)
-
-    # Visit right subtree
     postorder(root.right)
-
-    # Visit root
     print(root.data, end=" ")
-
-
-# Display result
 print("Postorder traversal:")
 postorder(root)
