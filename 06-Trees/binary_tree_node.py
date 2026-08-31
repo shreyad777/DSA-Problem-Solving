2b5ff9a483191537e29fd77b816b46fd20ceef8b
shreyad777 <shreyagowdad852@gmail.com>
2026-08-31 11:57:07 +0530

    Modified the existing solution and commited the updated changes

--- 06-Trees/binary_tree_node.py
+++ 06-Trees/binary_tree_node.py
@@ -1,24 +1,15 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-
-# Create root
 root = Node(10)
-
-# Add children
 root.left = Node(5)
 root.right = Node(20)
-
-# Add children of 5
 root.left.left = Node(3)
 root.left.right = Node(7)
 
-
-# Display tree values
 print("Root:", root.data)
 print("Left child:", root.left.data)
 print("Right child:", root.right.data)
