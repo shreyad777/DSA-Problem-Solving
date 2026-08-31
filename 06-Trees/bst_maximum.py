f8cd2577f1a58d69823b8157ae3f0c7c36331b80
shreyad777 <shreyagowdad852@gmail.com>
2026-08-31 12:04:10 +0530

    Modified the existing solution and commited the updated changes

--- 06-Trees/bst_maximum.py
+++ 06-Trees/bst_maximum.py
@@ -4,9 +4,6 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-
-
-# Insert into BST
 def insert(root, value):
 
     if root is None:
@@ -20,8 +17,6 @@ def insert(root, value):
 
     return root
 
-
-# Find maximum value
 def find_max(root):
 
     if root is None:
@@ -34,17 +29,11 @@ def find_max(root):
 
     return current.data
 
-
-# Create BST
 root = None
 
 values = [50, 30, 70, 20, 40, 60, 80]
 
 for value in values:
     root = insert(root, value)
-
-
-# Find maximum
 maximum = find_max(root)
-
 print("Maximum value:", maximum)
