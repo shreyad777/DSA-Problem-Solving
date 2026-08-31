e6170ffb0038189e43eb9b480a7c2540ec0e231f
shreyad777 <shreyagowdad852@gmail.com>
2026-08-31 12:08:05 +0530

    Modified the existing solution and commited the updated changes

--- 06-Trees/height_of_tree.py
+++ 06-Trees/height_of_tree.py
@@ -1,12 +1,8 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
-
-# Create tree
 root = Node(10)
 
 root.left = Node(5)
@@ -15,8 +11,6 @@ root.right = Node(20)
 root.left.left = Node(3)
 root.left.right = Node(7)
 
-
-# Find height
 def height(root):
 
     if root is None:
@@ -27,8 +21,6 @@ def height(root):
 
     return 1 + max(left_height, right_height)
 
-
-# Display result
 tree_height = height(root)
 
 print("Height of tree:", tree_height)
