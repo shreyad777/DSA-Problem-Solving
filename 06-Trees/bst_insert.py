cf18141338b5c502c19ddd99fd411cca8d155c5d
shreyad777 <shreyagowdad852@gmail.com>
2026-08-31 12:01:52 +0530

    Modified the existing solution and commited the updated changes

--- 06-Trees/bst_insert.py
+++ 06-Trees/bst_insert.py
@@ -4,9 +4,6 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-
-
-# Insert a value into BST
 def insert(root, value):
 
     if root is None:
@@ -19,9 +16,6 @@ def insert(root, value):
         root.right = insert(root.right, value)
 
     return root
-
-
-# Inorder traversal
 def inorder(root):
 
     if root is None:
@@ -30,9 +24,6 @@ def inorder(root):
     inorder(root.left)
     print(root.data, end=" ")
     inorder(root.right)
-
-
-# Create BST
 root = None
 
 root = insert(root, 50)
@@ -41,7 +32,5 @@ root = insert(root, 70)
 root = insert(root, 20)
 root = insert(root, 40)
 
-
-# Display BST
 print("BST in sorted order:")
 inorder(root)
