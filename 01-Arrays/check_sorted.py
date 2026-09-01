5e1c284b0517ccc68bfe40b505a1c3a0de67c52d
shreyad777 <shreyagowdad852@gmail.com>
2026-09-01 12:15:23 +0530

    Updated the modified solution

--- 01-Arrays/check_sorted.py
+++ 01-Arrays/check_sorted.py
@@ -1,15 +1,10 @@
 def is_sorted(arr):
-
     for i in range(len(arr) - 1):
-
         if arr[i] > arr[i + 1]:
             return False
-
     return True
 
-
 arr = [1, 2, 3, 4, 5]
-
 if is_sorted(arr):
     print("Array is sorted")
 else:
