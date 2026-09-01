13565c06e4eb64cc94c36da3922c0477c9f69694
shreyad777 <shreyagowdad852@gmail.com>
2026-09-01 12:23:00 +0530

    Added the updated solution

--- 01-Arrays/maximum_product_subarray.py
+++ 01-Arrays/maximum_product_subarray.py
@@ -1,41 +1,27 @@
-# Question 121: Maximum Product Subarray
-
-
 def max_product(nums):
-
     current_max = nums[0]
     current_min = nums[0]
-
     result = nums[0]
-
     for i in range(1, len(nums)):
-
         num = nums[i]
-
-        # Save old values before updating
+       
         previous_max = current_max
         previous_min = current_min
-
         current_max = max(
             num,
             num * previous_max,
             num * previous_min
         )
-
         current_min = min(
             num,
             num * previous_max,
             num * previous_min
         )
-
         result = max(
             result,
             current_max
         )
 
     return result
-
-
 nums = [2, 3, -2, 4]
-
 print("Maximum product:", max_product(nums))
