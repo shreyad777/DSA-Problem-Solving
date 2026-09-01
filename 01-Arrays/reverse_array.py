f8cf5e83a483842250e7a6d59f66d0950d039d7b
shreyad777 <shreyagowdad852@gmail.com>
2026-09-01 13:00:53 +0530

    Added the modified solution

--- 01-Arrays/reverse_array.py
+++ 01-Arrays/reverse_array.py
@@ -3,7 +3,6 @@ def reverse_array(arr):
     right = len(arr) - 1
     while left < right:
         arr[left], arr[right] = arr[right], arr[left]
-
         left += 1
         right -= 1
     return arr
