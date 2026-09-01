f1b071e84416ec2d3395e31d62610c097198e914
shreyad777 <shreyagowdad852@gmail.com>
2026-09-01 13:01:39 +0530

    Adeed the modified solution

--- 01-Arrays/second_largest.py
+++ 01-Arrays/second_largest.py
@@ -1,21 +1,14 @@
 def find_second_largest(arr):
     largest = float('-inf')
     second_largest = float('-inf')
-
     for num in arr:
         if num > largest:
             second_largest = largest
             largest = num
-
         elif num > second_largest and num != largest:
             second_largest = num
-
     return second_largest
-
-
 arr = [10, 5, 25, 8, 30, 15]
-
 result = find_second_largest(arr)
-
 print("Array:", arr)
 print("Second largest element:", result)
