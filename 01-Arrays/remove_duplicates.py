ebd0b24d18fe82fe6444e0191c317f0626ef639d
shreyad777 <shreyagowdad852@gmail.com>
2026-09-01 12:59:10 +0530

    Added the new solution

--- 01-Arrays/remove_duplicates.py
+++ 01-Arrays/remove_duplicates.py
@@ -1,22 +1,12 @@
 def remove_duplicates(arr):
-
     if len(arr) == 0:
         return 0
-
     i = 0
-
     for j in range(1, len(arr)):
-
         if arr[j] != arr[i]:
-
             i += 1
             arr[i] = arr[j]
-
     return i + 1
-
-
 arr = [1, 1, 2, 2, 3, 4, 4, 5]
-
 new_length = remove_duplicates(arr)
-
 print("Array after removing duplicates:", arr[:new_length])
