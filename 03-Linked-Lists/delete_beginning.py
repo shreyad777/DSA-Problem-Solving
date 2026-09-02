390944659acc174a17ecaa86d1a762c409dbc7ed
shreyad777 <shreyagowdad852@gmail.com>
2026-09-02 23:25:14 +0530

    Updated the modified solution

--- 03-Linked-Lists/delete_beginning.py
+++ 03-Linked-Lists/delete_beginning.py
@@ -1,19 +1,11 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.next = None
-
-
 def delete_beginning(head):
-
     if head is None:
         return None
-
     return head.next
-
-
-# Create linked list
 node1 = Node(10)
 node2 = Node(20)
 node3 = Node(30)
@@ -24,17 +16,9 @@ node2.next = node3
 node3.next = node4
 
 head = node1
-
-
-# Delete the first node
 head = delete_beginning(head)
-
-
-# Traverse the linked list
 current = head
 
 while current is not None:
-
     print(current.data, end=" ")
-
     current = current.next
