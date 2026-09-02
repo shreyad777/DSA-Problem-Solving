ffdb5258104203b948be60550e2add659e58275b
shreyad777 <shreyagowdad852@gmail.com>
2026-09-02 22:35:28 +0530

    Added the modified solution

--- 02-Strings/longest_palindromic_subsequence.py
+++ 02-Strings/longest_palindromic_subsequence.py
@@ -1,36 +1,22 @@
 def longest_palindromic_subsequence(s):
     reversed_s = s[::-1]
-
     n = len(s)
-
-    # DP table
     dp = [
         [0] * (n + 1)
         for _ in range(n + 1)
     ]
-
     for i in range(1, n + 1):
-
         for j in range(1, n + 1):
-
             if s[i - 1] == reversed_s[j - 1]:
-
                 dp[i][j] = (
                     dp[i - 1][j - 1] + 1
                 )
-
             else:
-
                 dp[i][j] = max(
                     dp[i - 1][j],
                     dp[i][j - 1]
                 )
-
     return dp[n][n]
-
-
 s = "bbbab"
-
 result = longest_palindromic_subsequence(s)
-
 print("Longest palindromic subsequence:", result)
