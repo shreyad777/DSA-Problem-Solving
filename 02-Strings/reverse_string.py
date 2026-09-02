20ec6091ceef4d408b5506637393aa69d00eecd3
shreyad777 <shreyagowdad852@gmail.com>
2026-09-02 23:15:39 +0530

    Updated the solution

--- 02-Strings/reverse_string.py
+++ 02-Strings/reverse_string.py
@@ -1,16 +1,9 @@
 def reverse_string(text):
-
     reversed_text = ""
-
     for i in range(len(text) - 1, -1, -1):
         reversed_text += text[i]
-
     return reversed_text
-
-
 text = "hello"
-
 result = reverse_string(text)
-
 print("Original string:", text)
 print("Reversed string:", result)
