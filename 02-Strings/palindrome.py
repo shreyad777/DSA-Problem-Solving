3808e222da8a8a8fbd4a769dc9075b12efc2e2e0
shreyad777 <shreyagowdad852@gmail.com>
2026-09-02 23:13:44 +0530

    Updated the solution

--- 02-Strings/palindrome.py
+++ 02-Strings/palindrome.py
@@ -1,19 +1,13 @@
 def is_palindrome(text):
     left = 0
     right = len(text) - 1
-
     while left < right:
         if text[left] != text[right]:
             return False
-
         left += 1
         right -= 1
-
     return True
-
-
 text = "madam"
-
 if is_palindrome(text):
     print("Palindrome")
 else:
