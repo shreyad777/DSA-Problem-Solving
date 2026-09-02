12c467f31c6c12dc582bc140d0b63942ba409a50
shreyad777 <shreyagowdad852@gmail.com>
2026-09-02 23:17:32 +0530

    Updated the modified solution

--- 02-Strings/reverse_words.py
+++ 02-Strings/reverse_words.py
@@ -1,13 +1,7 @@
 def reverse_words(text):
-
     words = text.split()
-
     return " ".join(words[::-1])
-
-
 text = "I love Python"
-
 result = reverse_words(text)
-
 print("Original sentence:", text)
 print("Reversed words:", result)
