a911a0e114ff5de836e7388bcebad5076627c6af
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:36:26 +0530

    Commit changes regarding the new updated solution

--- 04-Stack/sort_stack.py
+++ 04-Stack/sort_stack.py
@@ -1,21 +1,12 @@
 def sort_stack(stack):
-
     temp_stack = []
-
     while stack:
-
         current = stack.pop()
-
         while temp_stack and temp_stack[-1] > current:
             stack.append(temp_stack.pop())
-
         temp_stack.append(current)
-
     return temp_stack
-
 stack = [30, 10, 50, 20, 40]
-
 sorted_stack = sort_stack(stack)
-
 print("Original stack:", stack)
 print("Sorted stack:", sorted_stack)
