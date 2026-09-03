954ce0d517650b6aa3df5fc3580b1fa24c886861
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:26:23 +0530

    Updated the modified solution

--- 03-Linked-Lists/reverse_list.py
+++ 03-Linked-Lists/reverse_list.py
@@ -1,29 +1,16 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.next = None
-
-
 def reverse_list(head):
-
     previous = None
     current = head
-
     while current is not None:
-
         next_node = current.next
-
         current.next = previous
-
         previous = current
-
         current = next_node
-
     return previous
-
-
-# Create linked list
 node1 = Node(10)
 node2 = Node(20)
 node3 = Node(30)
@@ -34,17 +21,9 @@ node2.next = node3
 node3.next = node4
 
 head = node1
-
-
-# Reverse the linked list
 head = reverse_list(head)
-
-
-# Display the reversed list
 current = head
 
 while current is not None:
-
     print(current.data, end=" ")
-
     current = current.next
