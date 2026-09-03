0a55f086a29f7c2c088cac021bec436ff44f5b6e
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:39:35 +0530

    Added thr updated solution and commited changes

--- 04-Stack/stack_using_queues.py
+++ 04-Stack/stack_using_queues.py
@@ -1,35 +1,21 @@
 from collections import deque
-
 class StackUsingQueue:
     def __init__(self):
         self.q = deque()
-
-    # Push element into stack
     def push(self, x):
         self.q.append(x)
-
-        # Move all previous elements behind the new element
         for _ in range(len(self.q) - 1):
             self.q.append(self.q.popleft())
-
-    # Remove top element
     def pop(self):
         if not self.q:
             return "Stack is empty"
         return self.q.popleft()
-
-    # Return top element
     def peek(self):
         if not self.q:
             return "Stack is empty"
         return self.q[0]
-
-    # Check if stack is empty
     def is_empty(self):
         return len(self.q) == 0
-
-
-# Example
 stack = StackUsingQueue()
 
 stack.push(10)
