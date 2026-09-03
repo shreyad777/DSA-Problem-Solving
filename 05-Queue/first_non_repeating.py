f80ca4458598698e1e10441cdbe3ec11a8b53955
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:43:46 +0530

    Added the updated solution

--- 05-Queue/first_non_repeating.py
+++ 05-Queue/first_non_repeating.py
@@ -1,36 +1,19 @@
 from collections import deque
-
-
 def first_non_repeating(string):
-
     frequency = {}
     queue = deque()
-
-    # Count frequencies
     for char in string:
-
         if char not in frequency:
             frequency[char] = 1
         else:
             frequency[char] += 1
-
-    # Add non-repeating characters
     for char in string:
-
         if frequency[char] == 1:
             queue.append(char)
-
-    # Return first non-repeating character
     if queue:
         return queue[0]
-
     return None
-
-
-# Test
 string = "aabbcdde"
-
 result = first_non_repeating(string)
-
 print("String:", string)
 print("First non-repeating character:", result)
