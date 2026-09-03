def9daa38d4a73ec0ec035608cea1ce43fc3560a
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:44:52 +0530

    Added the solution

--- 05-Queue/generate_binary_numbers.py
+++ 05-Queue/generate_binary_numbers.py
@@ -1,35 +1,19 @@
 from collections import deque
-
-
 def generate_binary(n):
-
     if n <= 0:
         return []
-
     queue = deque()
     queue.append("1")
-
     result = []
-
     for _ in range(n):
-
         current = queue.popleft()
-
         result.append(current)
-
         queue.append(current + "0")
         queue.append(current + "1")
-
     return result
-
-
-# Test
 n = 5
-
 result = generate_binary(n)
-
 print("N:", n)
 print("Binary numbers:")
-
 for number in result:
     print(number)
