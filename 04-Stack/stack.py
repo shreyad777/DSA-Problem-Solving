774f005691147c7b3600cd770ca5ead578b8416d
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:40:27 +0530

    Added a solution

--- 04-Stack/stack.py
+++ 04-Stack/stack.py
@@ -1,29 +1,15 @@
 stack = []
-
-
-# Push elements
 stack.append(10)
 stack.append(20)
 stack.append(30)
-
 print("Stack:", stack)
-
-
-# Pop the top element
 removed = stack.pop()
-
 print("Removed:", removed)
 print("Stack after pop:", stack)
-
-
-# Peek at the top element
 if stack:
     print("Top element:", stack[-1])
 else:
     print("Stack is empty")
-
-
-# Check whether stack is empty
 if not stack:
     print("Stack is empty")
 else:
