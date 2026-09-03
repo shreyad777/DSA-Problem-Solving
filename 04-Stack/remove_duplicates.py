6a3d192938590c51641b128a49c65692566ba0fc
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:34:06 +0530

    Added the modified and updated solution

--- 04-Stack/remove_duplicates.py
+++ 04-Stack/remove_duplicates.py
@@ -1,23 +1,14 @@
 def remove_duplicates(string):
-
     stack = []
-
     for char in string:
-
         if not stack:
             stack.append(char)
-
         elif stack[-1] == char:
             stack.pop()
-
         else:
             stack.append(char)
-
     return ''.join(stack)
-
 string = "abbaca"
-
 result = remove_duplicates(string)
-
 print("Original:", string)
 print("After removing duplicates:", result)
