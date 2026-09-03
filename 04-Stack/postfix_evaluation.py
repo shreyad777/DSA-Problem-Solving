6c63340a09fae2ccfe3b0b5c9504ae1eed897e3e
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:32:44 +0530

    Added modified solution

--- 04-Stack/postfix_evaluation.py
+++ 04-Stack/postfix_evaluation.py
@@ -1,35 +1,21 @@
 def evaluate_postfix(expression):
-
     stack = []
-
     for item in expression.split():
-
         if item.isdigit():
-
             stack.append(int(item))
-
         else:
-
             b = stack.pop()
             a = stack.pop()
-
             if item == '+':
                 stack.append(a + b)
-
             elif item == '-':
                 stack.append(a - b)
-
             elif item == '*':
                 stack.append(a * b)
-
             elif item == '/':
                 stack.append(a / b)
-
     return stack[0]
-
 expression = "2 3 +"
-
 result = evaluate_postfix(expression)
-
 print("Expression:", expression)
 print("Result:", result)
