913cbe97cb7d355fe5872a75f08c2e784299dc1a
shreyad777 <shreyagowdad852@gmail.com>
2026-09-03 21:29:27 +0530

    Added the modified changes and updated the solution

--- 04-Stack/balanced_parentheses.py
+++ 04-Stack/balanced_parentheses.py
@@ -1,25 +1,14 @@
 def is_balanced(expression):
-
     stack = []
-
     for char in expression:
-
         if char == '(':
             stack.append(char)
-
         elif char == ')':
-
             if not stack:
                 return False
-
             stack.pop()
-
     return len(stack) == 0
-
-
-# Test expression
 expression = "(a + b)"
-
 if is_balanced(expression):
     print("Balanced")
 else:
