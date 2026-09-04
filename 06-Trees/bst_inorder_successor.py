1f3531ccacfad6ec1c750923be5d7b30c346e485
shreyad777 <shreyagowdad852@gmail.com>
2026-09-04 20:52:04 +0530

    Added and updated a new solution

--- 06-Trees/bst_inorder_successor.py
+++ 06-Trees/bst_inorder_successor.py
@@ -4,57 +4,36 @@ class Node:
         self.left = None
         self.right = None
 def insert(root, value):
-
     if root is None:
         return Node(value)
-
     if value < root.data:
         root.left = insert(root.left, value)
-
     elif value > root.data:
         root.right = insert(root.right, value)
-
     return root
-
 def inorder_successor(root, target):
-
     successor = None
     current = root
-
     while current is not None:
-
         if target < current.data:
             successor = current
             current = current.left
-
         elif target > current.data:
             current = current.right
-
         else:
-            # Target has a right subtree
             if current.right is not None:
-
                 current = current.right
-
                 while current.left is not None:
                     current = current.left
-
                 successor = current
-
             break
-
     return successor
-
 root = None
 values = [50, 30, 70, 20, 40, 60, 80]
-
 for value in values:
     root = insert(root, value)
-
 target = 50
-
 successor = inorder_successor(root, target)
-
 if successor:
     print("Inorder successor of", target, "is", successor.data)
 else:
