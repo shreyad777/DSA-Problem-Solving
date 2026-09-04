637f8539abbc2d8e6e5d4ce473bb5ea60de020f7
shreyad777 <shreyagowdad852@gmail.com>
2026-09-04 20:46:21 +0530

    Added the new solution

--- 06-Trees/binary_tree_node.py
+++ 06-Trees/binary_tree_node.py
@@ -3,13 +3,11 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-
 root = Node(10)
 root.left = Node(5)
 root.right = Node(20)
 root.left.left = Node(3)
 root.left.right = Node(7)
-
 print("Root:", root.data)
 print("Left child:", root.left.data)
 print("Right child:", root.right.data)
