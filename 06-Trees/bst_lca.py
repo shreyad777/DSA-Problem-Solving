605251cb8fdcac6798fc9bf3b4812befc6926abb
shreyad777 <shreyagowdad852@gmail.com>
2026-09-04 20:54:48 +0530

    Added a updated solution and commited changes

--- 06-Trees/bst_lca.py
+++ 06-Trees/bst_lca.py
@@ -1,5 +1,4 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
@@ -7,42 +6,28 @@ class Node:
 def insert(root, value):
     if root is None:
         return Node(value)
-
     if value < root.data:
         root.left = insert(root.left, value)
-
     elif value > root.data:
         root.right = insert(root.right, value)
-
     return root
-
 def find_lca(root, p, q):
-
     current = root
-
     while current is not None:
-
         if p < current.data and q < current.data:
             current = current.left
-
         elif p > current.data and q > current.data:
             current = current.right
-
         else:
             return current
-
     return None
 root = None
-
 values = [50, 30, 70, 20, 40, 60, 80]
-
 for value in values:
     root = insert(root, value)
 p = 20
 q = 40
-
 lca = find_lca(root, p, q)
-
 if lca:
     print("LCA of", p, "and", q, "is", lca.data)
 else:
