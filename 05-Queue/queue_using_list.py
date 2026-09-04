1e9768ca31b99e2f403f5b04097d6ca55da3c4e2
shreyad777 <shreyagowdad852@gmail.com>
2026-09-04 20:30:55 +0530

    Modified and updated the solution

--- 05-Queue/queue_using_list.py
+++ 05-Queue/queue_using_list.py
@@ -2,21 +2,14 @@ queue = []
 queue.append(10)
 queue.append(20)
 queue.append(30)
-
 print("Queue:", queue)
-
 removed = queue.pop(0)
-
 print("Removed:", removed)
-
-
 print("Queue after dequeue:", queue)
-
 if queue:
     print("Front:", queue[0])
 else:
     print("Queue is empty")
-
 if not queue:
     print("Queue is empty")
 else:
