b0ecda97b6f9a3f9b61acd621e5690dd5ee9295c
shreyad777 <shreyagowdad852@gmail.com>
2026-09-04 20:48:09 +0530

    Updated the new solution

--- 06-Trees/bst_height.py
+++ 06-Trees/bst_height.py
@@ -4,35 +4,22 @@ class Node:
         self.left = None
         self.right = None
 def insert(root, value):
-
     if root is None:
         return Node(value)
-
     if value < root.data:
         root.left = insert(root.left, value)
-
     elif value > root.data:
         root.right = insert(root.right, value)
-
     return root
-
 def height(root):
-
     if root is None:
         return 0
-
     left_height = height(root.left)
     right_height = height(root.right)
-
     return 1 + max(left_height, right_height)
-
 root = None
-
 values = [50, 30, 70, 20, 40, 60, 80]
-
 for value in values:
     root = insert(root, value)
-
 tree_height = height(root)
-
 print("Height of BST:", tree_height)
