b77a9438f284d5b3fd7513943d689543f3e68c6d
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 21:52:03 +0530

    Added the new solution

--- 06-Trees/level_order_traversal.py
+++ 06-Trees/level_order_traversal.py
@@ -1,36 +1,24 @@
 from collections import deque
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 root = Node(10)
-
 root.left = Node(5)
 root.right = Node(20)
-
 root.left.left = Node(3)
 root.left.right = Node(7)
-
 def level_order(root):
-
     if root is None:
         return
-
     queue = deque()
-
     queue.append(root)
-
     while queue:
-
         current = queue.popleft()
-
         print(current.data, end=" ")
-
         if current.left:
             queue.append(current.left)
-
         if current.right:
             queue.append(current.right)
 print("Level order traversal:")
