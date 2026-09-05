f3a902964d8da17d429e2f2ad67d0e189cb5f72a
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 22:05:34 +0530

    Updated the new mof=dified solution

--- 07-Hashing/find_duplicates.py
+++ 07-Hashing/find_duplicates.py
@@ -1,17 +1,11 @@
 arr = [1, 2, 3, 2, 4, 5, 1, 3]
-
 seen = set()
 duplicates = set()
-
 for num in arr:
-
     if num in seen:
         duplicates.add(num)
     else:
         seen.add(num)
-
-
 print("Duplicate elements:")
-
 for num in duplicates:
     print(num)
