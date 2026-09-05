b1ad070f2bd6b6313ddad658eeb94892c18774a2
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 21:57:02 +0530

    Added the modified solution

--- 06-Trees/preorder_traversal.py
+++ 06-Trees/preorder_traversal.py
@@ -4,15 +4,11 @@ class Node:
         self.left = None
         self.right = None
 root = Node(10)
-
 root.left = Node(5)
 root.right = Node(20)
-
 root.left.left = Node(3)
 root.left.right = Node(7)
-
 def preorder(root):
-
     if root is None:
         return
     print(root.data, end=" ")
