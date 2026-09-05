372e35694dd9b70099b08208cf19f7fa5ba0b1f9
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 21:55:12 +0530

    Updated a solution

--- 06-Trees/maximum_value.py
+++ 06-Trees/maximum_value.py
@@ -4,23 +4,15 @@ class Node:
         self.left = None
         self.right = None
 root = Node(10)
-
 root.left = Node(5)
 root.right = Node(20)
-
 root.left.left = Node(3)
 root.left.right = Node(7)
-
 def find_max(root):
-
     if root is None:
         return float("-inf")
-
     left_max = find_max(root.left)
     right_max = find_max(root.right)
-
     return max(root.data, left_max, right_max)
-
 maximum = find_max(root)
-
 print("Maximum value:", maximum)
