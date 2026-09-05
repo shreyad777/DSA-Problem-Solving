90777eacfe10a23dc9059d2f28732597c293a3fd
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 21:46:37 +0530

    Added the modified solution

--- 06-Trees/bst_search.py
+++ 06-Trees/bst_search.py
@@ -4,40 +4,26 @@ class Node:
         self.left = None
         self.right = None
 def insert(root, value):
-
     if root is None:
         return Node(value)
-
     if value < root.data:
         root.left = insert(root.left, value)
-
     elif value > root.data:
         root.right = insert(root.right, value)
-
     return root
-
 def search(root, target):
-
     if root is None:
         return False
-
     if root.data == target:
         return True
-
     if target < root.data:
         return search(root.left, target)
-
     return search(root.right, target)
-
 root = None
-
 values = [50, 30, 70, 20, 40, 60, 80]
-
 for value in values:
     root = insert(root, value)
-
 target = 60
-
 if search(root, target):
     print(target, "found in BST")
 else:
