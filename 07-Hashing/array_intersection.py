fab5e66e1c80ca774de8977424c0b06629ea7ae7
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 22:02:30 +0530

    Commited new changes and updated the solution

--- 07-Hashing/array_intersection.py
+++ 07-Hashing/array_intersection.py
@@ -1,21 +1,11 @@
 def intersection(arr1, arr2):
-
     set1 = set(arr1)
     result = set()
-
     for num in arr2:
-
         if num in set1:
             result.add(num)
-
     return result
-
-
-# Example arrays
 arr1 = [1, 2, 3, 4, 5]
 arr2 = [3, 4, 5, 6, 7]
-
-
 result = intersection(arr1, arr2)
-
 print("Intersection:", result)
