e9f0a0cd32c51a74be263a1905b663cd58c8f14b
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 22:03:33 +0530

    Added new changes

--- 07-Hashing/array_union.py
+++ 07-Hashing/array_union.py
@@ -1,18 +1,11 @@
 def find_union(arr1, arr2):
-
     union = set()
-
     for num in arr1:
         union.add(num)
-
     for num in arr2:
         union.add(num)
-
     return union
-
 arr1 = [1, 2, 3, 4]
 arr2 = [3, 4, 5, 6]
-
 result = find_union(arr1, arr2)
-
 print("Union:", result)
