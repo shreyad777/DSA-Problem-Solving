5846262ad48085285839f64f63da7b8a0c32afcd
shreyad777 <shreyagowdad852@gmail.com>
2026-09-05 21:58:03 +0530

    Added the new solution

--- 06-Trees/search_binary_tree.py
+++ 06-Trees/search_binary_tree.py
@@ -1,36 +1,20 @@
 class Node:
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
-
-# Create tree
 root = Node(10)
-
 root.left = Node(5)
 root.right = Node(20)
-
 root.left.left = Node(3)
 root.left.right = Node(7)
-
-
-# Search for a value
 def search(root, target):
-
     if root is None:
         return False
-
     if root.data == target:
         return True
-
     return search(root.left, target) or search(root.right, target)
-
-
-# Test
 target = 7
-
 if search(root, target):
     print(target, "found in the tree")
 else:
