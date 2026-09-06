4af0e3c1fa55e0240af98d456bbf840ff81d254e
shreyad777 <shreyagowdad852@gmail.com>
2026-09-06 20:48:24 +0530

    Added a new solution

--- 07-Hashing/majority_element.py
+++ 07-Hashing/majority_element.py
@@ -1,25 +1,13 @@
 def majority_element(arr):
-
     frequency = {}
-
-    # Count frequency
     for num in arr:
         frequency[num] = frequency.get(num, 0) + 1
-
-    # Find majority element
     for num in frequency:
-
         if frequency[num] > len(arr) // 2:
             return num
-
     return None
-
-
-# Example
 arr = [2, 2, 1, 1, 1, 2, 2]
-
 result = majority_element(arr)
-
 if result is not None:
     print("Majority element:", result)
 else:
