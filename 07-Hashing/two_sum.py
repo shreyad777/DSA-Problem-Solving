624d54bf09905a21649b9818e795462a3e1b8c23
shreyad777 <shreyagowdad852@gmail.com>
2026-09-06 20:51:18 +0530

    Added a new solution

--- 07-Hashing/two_sum.py
+++ 07-Hashing/two_sum.py
@@ -1,16 +1,9 @@
 def two_sum(arr, target):
-
     seen = {}
-
     for i in range(len(arr)):
-
         current = arr[i]
-
         needed = target - current
-
         if needed in seen:
             return [seen[needed], i]
-
         seen[current] = i
-
     return []
