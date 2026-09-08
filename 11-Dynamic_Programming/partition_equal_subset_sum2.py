03c1d10b0073fb1058571b3715fcdc9854946dfe
shreyad777 <shreyagowdad852@gmail.com>
2026-09-09 04:48:57 +0530

    Added a new solution

--- 11-Dynamic_Programming/partition_equal_subset_sum2.py
+++ 11-Dynamic_Programming/partition_equal_subset_sum2.py
@@ -4,21 +4,17 @@ def can_partition(nums):
         return False
     target = total // 2
     dp = [False] * (target + 1)
-
     dp[0] = True
-
     for num in nums:
         for current_sum in range(
             target,
             num - 1,
             -1
         ):
-
             dp[current_sum] = (
                 dp[current_sum]
                 or dp[current_sum - num]
             )
-
     return dp[target]
 nums = [1, 5, 11, 5]
 print(
