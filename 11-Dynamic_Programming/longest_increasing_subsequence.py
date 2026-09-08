aae13badb59ebb67f56d4768780564b6f8ae1e97
shreyad777 <shreyagowdad852@gmail.com>
2026-09-08 21:24:22 +0530

    Updated and modified solution

--- 11-Dynamic_Programming/longest_increasing_subsequence.py
+++ 11-Dynamic_Programming/longest_increasing_subsequence.py
@@ -1,25 +1,16 @@
 def length_of_lis(nums):
-
     n = len(nums)
-
     if n == 0:
         return 0
     dp = [1] * n
-
     for i in range(n):
-
         for j in range(i):
-
             if nums[j] < nums[i]:
-
                 dp[i] = max(
                     dp[i],
                     dp[j] + 1
                 )
-
     return max(dp)
-
-
 nums = [10, 9, 2, 5, 3, 7, 101, 18]
 result = length_of_lis(nums)
 print("Length of LIS:", result)
