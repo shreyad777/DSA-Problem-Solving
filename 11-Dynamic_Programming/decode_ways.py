1f86696a16f1ce2151303933581628d891cc3369
shreyad777 <shreyagowdad852@gmail.com>
2026-09-08 21:15:02 +0530

    Updated the solution

--- 11-Dynamic_Programming/decode_ways.py
+++ 11-Dynamic_Programming/decode_ways.py
@@ -7,15 +7,10 @@ def num_decodings(s):
         current = 0
         if s[i] != '0':
             current += previous_one
-
         two_digit = int(s[i - 1:i + 1])
-
         if 10 <= two_digit <= 26:
             current += previous_two
-
         previous_two = previous_one
         previous_one = current
-
     return previous_one
-
 print(num_decodings("226"))
