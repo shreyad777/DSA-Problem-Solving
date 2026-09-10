827a612528850d867008daa6e42ea23aa98527c1
shreyad777 <shreyagowdad852@gmail.com>
2026-09-10 20:03:35 +0530

    Added a new solution

---
+++ 11-Dynamic_Programming/stock_iii.py
@@ -0,0 +1,15 @@
+def max_profit(prices):
+    if not prices:
+        return 0
+    buy1 = float("-inf")
+    sell1 = 0
+    buy2 = float("-inf")
+    sell2 = 0
+    for price in prices:
+        buy1 = max(buy1, -price)
+        sell1 = max(sell1, buy1 + price)
+        buy2 = max(buy2, sell1 - price)
+        sell2 = max(sell2, buy2 + price)
+    return sell2
+prices = list(map(int, input("Enter stock prices: ").split()))
+print("Maximum Profit:", max_profit(prices))
