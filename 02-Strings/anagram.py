3b678820983507cd8e50b503feecdf337ebc846d
shreyad777 <shreyagowdad852@gmail.com>
2026-09-11 22:15:16 +0530

    Added a new modified solution

--- 02-Strings/anagram.py
+++ 02-Strings/anagram.py
@@ -8,20 +8,14 @@ def are_anagrams(text1, text2):
             frequency1[char] += 1
         else:
             frequency1[char] = 1
-
     for char in text2:
-
         if char in frequency2:
             frequency2[char] += 1
         else:
             frequency2[char] = 1
-
     return frequency1 == frequency2
-
-
 text1 = "listen"
 text2 = "silent"
-
 if are_anagrams(text1, text2):
     print("Anagram")
 else:
