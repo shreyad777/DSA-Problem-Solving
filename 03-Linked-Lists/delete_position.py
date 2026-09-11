6d57d787a84ad3ff4b265a76e03db863bc25217b
shreyad777 <shreyagowdad852@gmail.com>
2026-09-11 22:19:36 +0530

    Added a new solution and commited it

--- 03-Linked-Lists/delete_position.py
+++ 03-Linked-Lists/delete_position.py
@@ -22,15 +22,11 @@ node1 = Node(10)
 node2 = Node(20)
 node3 = Node(30)
 node4 = Node(40)
-
 node1.next = node2
 node2.next = node3
 node3.next = node4
-
 head = node1
-
 head = delete_at_position(head, 2)
-
 current = head
 while current is not None:
     print(current.data, end=" ")
