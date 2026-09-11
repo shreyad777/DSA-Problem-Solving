6b55330a7a10c539943fade62acd025dc89c03c9
shreyad777 <shreyagowdad852@gmail.com>
2026-09-11 22:10:52 +0530

    Added and updated the solution

--- 01-Arrays/maximum_product_subarray.py
+++ 01-Arrays/maximum_product_subarray.py
@@ -4,7 +4,6 @@ def max_product(nums):
     result = nums[0]
     for i in range(1, len(nums)):
         num = nums[i]
-       
         previous_max = current_max
         previous_min = current_min
         current_max = max(
