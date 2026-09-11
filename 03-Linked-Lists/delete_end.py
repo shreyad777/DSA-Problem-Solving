525e47012f4451f7000b88e44089b1f9716b9afa
shreyad777 <shreyagowdad852@gmail.com>
2026-09-11 22:18:46 +0530

    Added a updated solution

--- 03-Linked-Lists/delete_end.py
+++ 03-Linked-Lists/delete_end.py
@@ -12,21 +12,16 @@ def delete_end(head):
         current = current.next
     current.next = None
     return head
-
 node1 = Node(10)
 node2 = Node(20)
 node3 = Node(30)
 node4 = Node(40)
-
 node1.next = node2
 node2.next = node3
 node3.next = node4
-
 head = node1
-
 head = delete_end(head)
-current = head
-
+current =head
 while current is not None:
     print(current.data, end=" ")
     current = current.next
