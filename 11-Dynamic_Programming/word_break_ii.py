b46c59c46e4dbe010d7f055aa03712d1c2edec2e
shreyad777 <shreyagowdad852@gmail.com>
2026-09-13 15:34:59 +0530

    Added and updated the new solution

---
+++ 11-Dynamic_Programming/word_break_ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordBreak(self, s, wordDict):
+        word_set = set(wordDict)
+        memo = {}
+        def dfs(start):
+            if start == len(s):
+                return [""]
+            if start in memo:
+                return memo[start]
+            result = []
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in word_set:
+                    remaining_sentences = dfs(end)
+                    for sentence in remaining_sentences:
+                        if sentence:
+                            result.append(word + " " + sentence)
+                        else:
+                            result.append(word)
+            memo[start] = result
+            return result
+        return dfs(0)
+s = input("Enter string: ")
+wordDict = input("Enter dictionary words: ").split()
+solution = Solution()
+result = solution.wordBreak(s, wordDict)
+print("Possible Sentences:")
+for sentence in result:
+    print(sentence)
