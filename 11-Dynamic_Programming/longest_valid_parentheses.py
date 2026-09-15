d5e37bbff23cda04f4c6a9d0b43d659891c998b5
shreyad777 <shreyagowdad852@gmail.com>
2026-09-15 14:30:38 +0530

    Added a new solution

---
+++ 11-Dynamic_Programming/longest_valid_parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        stack = [-1]
+        max_length = 0
+        for i, char in enumerate(s):
+            if char == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_length = max(
+                        max_length,
+                        i - stack[-1]
+                    )
+        return max_length
+s = input("Enter parentheses string: ")
+solution = Solution()
+print(
+    "Longest Valid Parentheses:",
+    solution.longestValidParentheses(s)
+)
