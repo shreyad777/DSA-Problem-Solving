c56e4fe8ea200339c6f59a06fa37db3da8c9f442
shreyad777 <shreyagowdad852@gmail.com>
2026-09-15 14:21:39 +0530

    Added the new solution and commited it

---
+++ 11-Dynamic_Programming/burst_balloons.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxCoins(self, nums):
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+        for length in range(2, n):
+            for left in range(n - length):
+                right = left + length
+                for k in range(left + 1, right):
+                    coins = (
+                        nums[left]
+                        * nums[k]
+                        * nums[right]
+                        + dp[left][k]
+                        + dp[k][right]
+                    )
+                    dp[left][right] = max(
+                        dp[left][right],
+                        coins
+                    )
+
+        return dp[0][n - 1]
+nums = list(map(int, input("Enter balloon values: ").split()))
+solution = Solution()
+print("Maximum Coins:", solution.maxCoins(nums))
