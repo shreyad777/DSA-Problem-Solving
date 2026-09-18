b1c347288542000e3c87ceb67a58f61263933f77
shreyad777 <shreyagowdad852@gmail.com>
2026-09-18 20:53:56 +0530

    Add Remove All Adjacent Duplicates II solution

--- 11-Dynamic_Programming/remove_duplicates_ii.py
+++ 11-Dynamic_Programming/remove_duplicates_ii.py
@@ -4,10 +4,8 @@ class Solution:
         for char in s:
             if stack and stack[-1][0] == char:
                 stack[-1][1] += 1
-
                 if stack[-1][1] == k:
                     stack.pop()
-
             else:
                 stack.append([char, 1])
 
