5589519c0b9841c8385a3000cf8774c7297f89e8
shreyad777 <shreyagowdad852@gmail.com>
2026-09-18 20:53:13 +0530

    Add Remove All Adjacent Duplicates II solution

---
+++ 11-Dynamic_Programming/remove_duplicates_ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeDuplicates(self, s, k):
+        stack = []
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+
+                if stack[-1][1] == k:
+                    stack.pop()
+
+            else:
+                stack.append([char, 1])
+
+        result = []
+
+        for char, count in stack:
+            result.append(char * count)
+
+        return "".join(result)
+
+
+s = input("Enter string: ")
+k = int(input("Enter k: "))
+
+solution = Solution()
+
+print(
+    "Result:",
+    solution.removeDuplicates(s, k)
+)
