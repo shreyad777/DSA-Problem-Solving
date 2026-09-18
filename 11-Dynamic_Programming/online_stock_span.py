2cef76cc12c1e5773f446303dffcf08285db6d44
shreyad777 <shreyagowdad852@gmail.com>
2026-09-18 20:48:48 +0530

    Add Online Stock Span solution

---
+++ 11-Dynamic_Programming/online_stock_span.py
@@ -0,0 +1,28 @@
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
+        span = 1
+
+        while self.stack and self.stack[-1][0] <= price:
+            previous_price, previous_span = self.stack.pop()
+            span += previous_span
+
+        self.stack.append((price, span))
+
+        return span
+
+
+prices = list(
+    map(int, input("Enter stock prices: ").split())
+)
+
+spanner = StockSpanner()
+
+result = []
+
+for price in prices:
+    result.append(spanner.next(price))
+
+print("Stock Spans:", result)
