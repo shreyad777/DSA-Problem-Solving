014d46fac34734865a65227139f350894696ef55
shreyad777 <shreyagowdad852@gmail.com>
2026-09-21 21:53:24 +0530

    Add 132 Pattern solution

--- 11-Dynamic_Programming/pattern.py
+++ 11-Dynamic_Programming/pattern.py
@@ -5,8 +5,6 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] < second:
                 return True
-
-            # Find a valid "2"
             while stack and nums[i] > stack[-1]:
                 second = stack.pop()
 
