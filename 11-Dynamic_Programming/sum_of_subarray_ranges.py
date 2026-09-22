41f7d22d73b6c1fdbca1c37cbd4d123e6da724f2
shreyad777 <shreyagowdad852@gmail.com>
2026-09-22 23:11:51 +0530

    Add Sum of Subarray Ranges solution

---
+++ 11-Dynamic_Programming/sum_of_subarray_ranges.py
@@ -0,0 +1,66 @@
+class Solution:
+    def sumSubarrayMins(self, nums):
+        n = len(nums)
+        stack = []
+        total = 0
+        for i in range(n + 1):
+            current = nums[i] if i < n else float("-inf")
+            while stack and nums[stack[-1]] > current:
+                mid = stack.pop()
+
+                left = (
+                    mid - stack[-1]
+                    if stack
+                    else mid + 1
+                )
+
+                right = i - mid
+
+                total += nums[mid] * left * right
+
+            stack.append(i)
+
+        return total
+
+    def sumSubarrayMaxs(self, nums):
+        n = len(nums)
+        stack = []
+        total = 0
+
+        for i in range(n + 1):
+            current = nums[i] if i < n else float("inf")
+
+            while stack and nums[stack[-1]] < current:
+                mid = stack.pop()
+
+                left = (
+                    mid - stack[-1]
+                    if stack
+                    else mid + 1
+                )
+
+                right = i - mid
+
+                total += nums[mid] * left * right
+
+            stack.append(i)
+
+        return total
+
+    def subArrayRanges(self, nums):
+        maximum_sum = self.sumSubarrayMaxs(nums)
+        minimum_sum = self.sumSubarrayMins(nums)
+
+        return maximum_sum - minimum_sum
+
+
+nums = list(
+    map(int, input("Enter array elements: ").split())
+)
+
+solution = Solution()
+
+print(
+    "Sum of Subarray Ranges:",
+    solution.subArrayRanges(nums)
+)
