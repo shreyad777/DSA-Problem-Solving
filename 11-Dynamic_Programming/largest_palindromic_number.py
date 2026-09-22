67a9c9a0847e8b0c26855e3e275d586c1524d39c
shreyad777 <shreyagowdad852@gmail.com>
2026-09-22 23:19:38 +0530

    Add Largest Palindromic Number solution

---
+++ 11-Dynamic_Programming/largest_palindromic_number.py
@@ -0,0 +1,43 @@
+class Solution:
+    def largestPalindromic(self, num):
+        count = [0] * 10
+        for digit in num:
+            count[int(digit)] += 1
+        left = []
+
+        # Build the left half from largest digit to smallest
+        for digit in range(9, -1, -1):
+            pairs = count[digit] // 2
+
+            # Don't allow leading zero
+            if digit == 0 and not left:
+                pairs = 0
+
+            left.append(str(digit) * pairs)
+            count[digit] -= pairs * 2
+
+        left = "".join(left)
+
+        # Find the largest remaining digit for the center
+        middle = ""
+
+        for digit in range(9, -1, -1):
+            if count[digit] > 0:
+                middle = str(digit)
+                break
+
+        # If everything was zero, return "0"
+        if not left:
+            return middle if middle else "0"
+
+        return left + middle + left[::-1]
+
+
+num = input("Enter digits: ")
+
+solution = Solution()
+
+print(
+    "Largest Palindromic Number:",
+    solution.largestPalindromic(num)
+)
